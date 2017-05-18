195e9e1c43499a19e814ceb8d9bf8fd95da0955a
SashaOcheev <OcheevSasha@gmail.com>
2017-05-18 08:36:50 +0300

    fix division

--- PP2/PP2.cpp
+++ PP2/PP2.cpp
@@ -39,8 +39,9 @@ int main(int argc, char* argv[])
     auto primitive = GetPrimitive(argv[1]);
 
 	CBank* bank = new CBank();
-
+    
     auto primitive = Primitive::CreatePrimitive(PrimitiveType::CRITICAL_SECTION);
+    
     bank->SetPrimitive(primitive.get());
 
 	auto client1 = bank->CreateClient();
